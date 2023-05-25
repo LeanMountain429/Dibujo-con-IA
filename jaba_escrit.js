4382127bfaec3cf14e8a6873a54cce0e42b102b2
LeanMountain429 <115753645+LeanMountain429@users.noreply.github.com>
2023-05-24 19:55:17 -0600

    Add files via upload

---
+++ jaba_escrit.js
@@ -0,0 +1,24 @@
+function preload(){
+    console.log("C ME ESCUCHA???? OLA???? ME OLLEN????? GUAU!! NO ME OLLEN!!!!! BYE!!!??")
+    img= loadImage("CUADRADO PRO.png")
+}
+function setup(){
+    camaweb= createCapture(VIDEO)
+    caanvaas= createCanvas(500,400)
+    camaweb.position(0,250)
+    caanvaas.position(1420,250)
+    neurored= ml5.poseNet(camaweb,chicargo)
+    neurored.on("pose",pooseeNeet)
+}
+function draw(){
+}
+function chicargo(){
+    console.log("CHI CARGO MENSO QUE NO VES???!!!")
+}
+function pooseeNeet(results){
+    console.log(results)
+    varnoseY= results[0].pose.nose.y
+    varnoseX= results[0].pose.nose.x
+    varleftwristX= results[0].pose.leftWrist.x
+    varrightwristX= results[0].pose.rightWrist.x
+}
